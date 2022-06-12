6f77375f6fc272a73cf992d97ed9bdf2d259e8c9
Iris System <iris@iris.ac.nz>
2022-06-11 22:34:14 -0700

    bootstrap: allow loading deployment-specific config file

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,10 @@
 <?php declare(strict_types=1);
 define("BASE_DIR", dirname(($p = realpath(__FILE__)) !== false ? $p : __FILE__));
 
+// load any deployment-specific config
+if (file_exists($config_path = BASE_DIR . '/config.inc.php'))
+	require_once($config_path);
+
 // load includes
 if (($sdi = glob(BASE_DIR . '/includes/*.inc.php')) !== false)
 	foreach ($sdi as $filename) require_once($filename);
